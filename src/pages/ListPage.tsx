8021cd5ce7eee0720eb44fb8abaedf809cf3d04a
Ferenandios <www.terunplay@gmail.com>
2025-11-15 18:44:18 +0300

    Created ListPage

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,13 +1,22 @@
 import { useEffect, type FC, type JSX } from "react";
 import { useAppDispatch } from "../hooks";
 import { getAds } from "../features/global.slice";
+import Title from "../components/ListPage/Title/Title";
+import Filters from "../components/ListPage/Filters/Filters";
+import Items from "../components/ListPage/items/Items";
 
 const ListPage: FC = (): JSX.Element => {
   const dispatch = useAppDispatch();
   useEffect(() => {
-    dispatch(getAds());
+    dispatch(getAds(1));
   }, [dispatch]);
-  return <div className="bg-lime-200">works!</div>;
+  return (
+    <main>
+      <Title />
+      <Filters />
+      <Items />
+    </main>
+  );
 };
 
 export default ListPage;
